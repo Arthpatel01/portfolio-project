1888aefabfc6d7072f4758bf641fb9ddbd74c65a
Arth Patel <arthpatel.ce@gmail.com>
2023-09-30 00:54:42 +0530

    New Temp

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from portfolio import views
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='portfolio.html')),
+    path('2', views.IndexView.as_view(), name="2")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,28 @@
+import os
+
+from django.http import FileResponse
 from django.shortcuts import render
+from django.views.generic import CreateView
+
 
 # Create your views here.
+
+class IndexView(CreateView):
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'home1.html', context)
+
+
+def download_file(request, file_path):
+    # Define the file path on your server
+    file_path = ''
+
+    # Extract the file name from the file path
+    filename = os.path.basename(file_path)
+
+    # Open the file for reading as binary
+    with open(file_path, 'rb') as file:
+        response = FileResponse(file)
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+        return response
